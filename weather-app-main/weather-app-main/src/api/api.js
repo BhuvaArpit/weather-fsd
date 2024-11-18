a21345d0aea9b11bb764b19072770a33a051f106
Arpitbhuva19 <arpitbhuva191@gmail.com>
2024-11-18 14:09:14 +0530

    weather_fsd

---
+++ weather-app-main/weather-app-main/src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '8c352d3ae5487b0a5301870e344bb64b';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
